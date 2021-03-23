b65601d4b4984b6ac89b6587b96134c3063aa919
Sandro Lex <sandro.lex@symphony.com>
2021-03-23 10:59:05 +0100

    add copy-users && compare-users\n add resquestPost

--- go/snykTool/search.go
+++ go/snykTool/search.go
@@ -1,254 +1,309 @@
 package snykTool
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "strings"
-    "time"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 var Debug bool
 var Timeout int
+var OrgsCache *OrgList
 
 func SetTimeout(t int) {
-    Timeout = t
+	Timeout = t
 }
 
 func GetTimeout() int {
-    if Timeout > 0 {
-        return Timeout
-    }
-    return 10
+	if Timeout > 0 {
+		return Timeout
+	}
+	return 10
 }
 
 func SetDebug(d bool) {
-    Debug = d
+	Debug = d
 }
 
 func IsDebug() bool {
-    return Debug
+	return Debug
 }
 
-func RequestGet(path string) (*http.Response) {
+func RequestGet(path string) *http.Response {
+	timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	req := SnykURL + path
+	if IsDebug() {
+		fmt.Println("GET", req)
+	}
+	request, err := http.NewRequest("GET", req, nil)
+	request.Header.Set("Authorization", "token "+GetToken())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
-    client := http.Client {
-        Timeout: timeout,
-    }
-    req := SnykURL + path
-    if IsDebug() {
-        fmt.Println(req)
-    }
-    request, err := http.NewRequest("GET", req, nil)
-    token := GetToken()
-    request.Header.Set("Authorization", "token " + token)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    resp, err := client.Do(request)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return resp
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
 }
 
+func RequestPost(path string, data []byte) *http.Response {
+	timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req := SnykURL + path
+	if IsDebug() {
+		fmt.Println("POST", req)
+	}
+
+	request, err := http.NewRequest("POST", req, bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "token "+GetToken())
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
+}
 
 func GetGroupMembers() ([]*User, error) {
-    group := GetGroupId()
-    resp := RequestGet("/group/" + group + "/members")
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        return nil, fmt.Errorf("GetGroupMembers failed %s", resp.Status)
-    }
-    var result []*User
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
-    resp.Body.Close()
-    return result, nil
+	path := fmt.Sprintf("/group/%s/members", GetGroupId())
+	resp := RequestGet(path)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("GetGroupMembers failed %s", resp.Status)
+	}
+	var result []*User
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return result, nil
 }
 
-
 func ListUsers(org_id string) ([]*User, error) {
-    resp := RequestGet("/org/" + org_id + "/members")
+	path := fmt.Sprintf("/org/%s/members", org_id)
+	resp := RequestGet(path)
 
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        return nil, fmt.Errorf("ListUsers failed %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("ListUsers failed %s", resp.Status)
+	}
 
-    var result []*User
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
+	var result []*User
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
 
-    resp.Body.Close()
-    return result, nil
+	resp.Body.Close()
+	return result, nil
+}
+
+func AddUser(org_id string, user_id string, role string) {
+	path := fmt.Sprintf("/group/%s/org/%s/members", GetGroupId(), org_id)
+	jsonValue, _ := json.Marshal(map[string]string{
+		"userId": user_id,
+		"role":   role,
+	})
+	resp := RequestPost(path, jsonValue)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		log.Fatal("Add User failed ", resp.Status)
+	}
+}
+
+func CopyUsers(o1, o2 string) {
+	// copy all users from o1 to o2
+	// dont check if it exists or if already present
+	// get users o1
+	// add to o2
+	result, err := ListUsers(o1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, user := range result {
+		AddUser(o2, user.Id, "collaborator")
+	}
 }
 
 func SearchProjects(org_id string, term string) (*ProjectsResult, error) {
-    result, err := GetProjects(org_id)
-    if err != nil {
-        log.Fatal(err)
-    }
+	result, err := GetProjects(org_id)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var filtered ProjectsResult
-    for _, prj := range(result.Projects) {
-        if  strings.Contains(strings.ToLower(prj.Name), strings.ToLower(term)) {
-           filtered.Projects = append(filtered.Projects, prj)
-        }
-    }
+	var filtered ProjectsResult
+	for _, prj := range result.Projects {
+		if strings.Contains(strings.ToLower(prj.Name), strings.ToLower(term)) {
+			filtered.Projects = append(filtered.Projects, prj)
+		}
+	}
 
-    return &filtered, nil
+	return &filtered, nil
 }
 
-func GetProjectIgnores(org_id string, prj_id string) ([]IgnoreResult) {
-    resp := RequestGet("/org/" + org_id + "/project/" + prj_id + "/ignores")
+func GetProjectIgnores(org_id string, prj_id string) []IgnoreResult {
+	path := fmt.Sprintf("/org/%s/project/%s/ignores", org_id, prj_id)
+	resp := RequestGet(path)
 
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        log.Fatal("Get Ignores failed ", resp.Status)
-    }
-    
-    var ignore_result map[string][]IgnoreStar
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		log.Fatal("Get Ignores failed ", resp.Status)
+	}
 
-    if err := json.NewDecoder(resp.Body).Decode(&ignore_result); err != nil {
-        resp.Body.Close()
-        log.Fatal(err)
-    }
+	var ignore_result map[string][]IgnoreStar
 
-    var result []IgnoreResult
+	if err := json.NewDecoder(resp.Body).Decode(&ignore_result); err != nil {
+		resp.Body.Close()
+		log.Fatal(err)
+	}
 
-    for key, value := range ignore_result {
-        for i := 0; i < len(value); i++ {
-            var ii IgnoreResult
-            ii.Id = key
-            ii.Content = value[i].Star
-            result = append(result, ii)
-        }
-    }
+	var result []IgnoreResult
 
-    resp.Body.Close()
-    return result
+	for key, value := range ignore_result {
+		for i := 0; i < len(value); i++ {
+			var ii IgnoreResult
+			ii.Id = key
+			ii.Content = value[i].Star
+			result = append(result, ii)
+		}
+	}
+
+	resp.Body.Close()
+	return result
 }
 
 func GetProjectIssues(org_id string, prj_id string) (*ProjectIssuesResult, error) {
-    timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
-    client := http.Client {
-        Timeout: timeout,
-    }
+	path := fmt.Sprintf("/org/%s/project/%s/aggregated-issues", org_id, prj_id)
+	resp := RequestPost(path, nil)
 
-    request, err := http.NewRequest("POST", SnykURL + "/org/" + org_id + "/project/" + prj_id + "/aggregated-issues", nil)
-    token := GetToken()
-    request.Header.Set("Content-Type", "application/json")
-    request.Header.Set("Authorization", "token " + token)
-    if err != nil {
-        log.Fatal(err)
-    }
-    resp, err := client.Do(request)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        return nil, fmt.Errorf("GetProjectIssues failed %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("GetProjectIssues failed %s", resp.Status)
+	}
 
-    var result ProjectIssuesResult
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
-    resp.Body.Close()
-    return &result, nil
+	var result ProjectIssuesResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
 }
 
-
 func GetProjects(org_id string) (*ProjectsResult, error) {
-    resp := RequestGet("/org/" + org_id + "/projects")
+	path := fmt.Sprintf("/org/%s/projects", org_id)
+	resp := RequestGet(path)
 
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        return nil, fmt.Errorf("GetProjects failed %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("GetProjects failed %s", resp.Status)
+	}
 
-    var result ProjectsResult
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
+	var result ProjectsResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
 
-    resp.Body.Close()
-    return &result, nil
+	resp.Body.Close()
+	return &result, nil
 }
 
 func CreateOrg(org_name string) (*CreateOrgResult, error) {
-    timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
-    client := http.Client{
-        Timeout: timeout,
-    }
-    jsonValue, _ := json.Marshal(map[string]string{
-        "name": org_name,
-    })
+	timeout := time.Duration(time.Duration(GetTimeout()) * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	jsonValue, _ := json.Marshal(map[string]string{
+		"name": org_name,
+	})
 
-    request, err := http.NewRequest("POST", SnykURL + "/org", bytes.NewBuffer(jsonValue))
-    request.Header.Set("Content-Type", "application/json")
-    token := GetToken()
-    request.Header.Set("Authorization", "token " + token)
-    resp, err := client.Do(request)
-    if err != nil {
-        return nil, err
-    }
-    if resp.StatusCode != http.StatusCreated {
-        resp.Body.Close()
-        return nil, fmt.Errorf("CreateOrg failed %s", resp.Status)
-    }
+	request, err := http.NewRequest("POST", SnykURL+"/org", bytes.NewBuffer(jsonValue))
+	request.Header.Set("Content-Type", "application/json")
+	token := GetToken()
+	request.Header.Set("Authorization", "token "+token)
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		resp.Body.Close()
+		return nil, fmt.Errorf("CreateOrg failed %s", resp.Status)
+	}
 
-    var result CreateOrgResult
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
-    resp.Body.Close()
-    return &result, nil
+	var result CreateOrgResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
 
 }
 
 func GetOrgs() (*OrgList, error) {
-    resp := RequestGet("/orgs")
- 
-    if resp.StatusCode != http.StatusOK {
-        resp.Body.Close()
-        return nil, fmt.Errorf("GetOrgs failed %s", resp.Status)
-    }
+	if OrgsCache != nil {
+		return OrgsCache, nil
+	}
 
-    var result OrgList
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        resp.Body.Close()
-        return nil, err
-    }
+	resp := RequestGet("/orgs")
 
-    resp.Body.Close()
-    return &result, nil
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("GetOrgs failed %s", resp.Status)
+	}
+
+	var result OrgList
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+
+	resp.Body.Close()
+	OrgsCache = &result
+	return &result, nil
+}
+
+func GetOrgName(id string) string {
+	orgs, err := GetOrgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, o := range orgs.Orgs {
+		if o.Id == id {
+			return o.Name
+		}
+	}
+	return ""
 }
 
 func SearchOrgs(term string) (*OrgList, error) {
-    result, err := GetOrgs()
-    if err != nil {
-        log.Fatal(err)
-    }
-    var filtered OrgList
-    for _, org := range(result.Orgs) {
-        if  strings.Contains(strings.ToLower(org.Name), strings.ToLower(term)) {
-           filtered.Orgs = append(filtered.Orgs, org)
-        }
-    }
-    return &filtered, nil
+	result, err := GetOrgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var filtered OrgList
+	for _, org := range result.Orgs {
+		if strings.Contains(strings.ToLower(org.Name), strings.ToLower(term)) {
+			filtered.Orgs = append(filtered.Orgs, org)
+		}
+	}
+	return &filtered, nil
 }
-
--- go/snykctl.go
+++ go/snykctl.go
@@ -1,267 +1,290 @@
 package main
 
 import (
-    "bufio"
-    "flag"
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-    "snykTool"
-    "sort"
-    "time"
+	"snykctl/snykTool"
+	"sort"
+	"time"
 )
 
 func usage() {
-    fmt.Printf("Usage:\n" +
-                "\tconfigure\n" +
-                "\tlist-users [org] [prj]\n" +
-                "\tlist-group-users\n" +
-                "\tlist-orgs\n" +
-                "\tsearch-org [name]\n" +
-                "\tcreate-org [name]\n" +
-                "\tlist-projects [org]\n" +
-                "\tsearch-projects [org]\n" +
-                "\tlist-project-issues [org] [prj]\n" +
-                "\treport-org-issues [org]\n")
+	fmt.Printf("Usage:\n" +
+		"\tconfigure\n" +
+		"\ttoken\n" +
+		"\tlist-users [org] [prj]\n" +
+		"\tlist-group-users\n" +
+		"\tcompare-users [org] [org]\n" +
+		"\tcopy-users [org] [org]\n" +
+		"\tlist-orgs\n" +
+		"\tsearch-org [name]\n" +
+		"\tcreate-org [name]\n" +
+		"\tlist-projects [org]\n" +
+		"\tsearch-projects [org]\n" +
+		"\tlist-project-issues [org] [prj]\n" +
+		"\treport-org-issues [org]\n" +
+		"\tlist-group-ignores\n" +
+		"\tlist-org-ignores [org]\n" +
+		"\tlist-project-ignores [org] [prj]\n")
 }
 
 func main() {
 	if len(os.Args) < 2 {
-        usage()
+		usage()
 		os.Exit(1)
 	}
-    quietFlag := flag.Bool("q", false, "Quiet output")
-    nameOnlyFlag := flag.Bool("n", false, "Names only output")
-    debugFlag := flag.Bool("d", false, "Debug http requests")
-    timeoutFlag := flag.Int("t", 10, "Http timeout")
-    flag.Parse()
-    if *debugFlag {
-        snykTool.SetDebug(true)
-    }
+	quietFlag := flag.Bool("q", false, "Quiet output")
+	nameOnlyFlag := flag.Bool("n", false, "Names only output")
+	debugFlag := flag.Bool("d", false, "Debug http requests")
+	timeoutFlag := flag.Int("t", 10, "Http timeout")
+	flag.Parse()
+	if *debugFlag {
+		snykTool.SetDebug(true)
+	}
 
-    if *quietFlag {
-        snykTool.SetQuiet(true)
-    }
+	if *quietFlag {
+		snykTool.SetQuiet(true)
+	}
 
-    if *nameOnlyFlag {
-        snykTool.SetNameOnly(true)
-    }
+	if *nameOnlyFlag {
+		snykTool.SetNameOnly(true)
+	}
 
-    if *timeoutFlag != 10 {
-        snykTool.SetTimeout(*timeoutFlag)
-    }
+	if *timeoutFlag != 10 {
+		snykTool.SetTimeout(*timeoutFlag)
+	}
 
 	switch flag.Arg(0) {
 	case "configure":
-        token := snykTool.GetToken()
-        group_id := snykTool.GetGroupId()
+		token := snykTool.GetToken()
+		group_id := snykTool.GetGroupId()
 
-        reader := bufio.NewReader(os.Stdin)
-        fmt.Printf("token [.... %s]: ", token[len(token)-6:])
-        text, _ := reader.ReadString('\n')
-        if len(text) > 2 {
-            token = text[:len(text)-1]
-        }
-        fmt.Printf("group_id [.... %s]: ", group_id[len(group_id)- 6:])
-        text, _ = reader.ReadString('\n')
-        if len(text) > 2 {
-            group_id = text[:len(text) -1]
-        }
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("token [.... %s]: ", token[len(token)-6:])
+		text, _ := reader.ReadString('\n')
+		if len(text) > 2 {
+			token = text[:len(text)-1]
+		}
+		fmt.Printf("group_id [.... %s]: ", group_id[len(group_id)-6:])
+		text, _ = reader.ReadString('\n')
+		if len(text) > 2 {
+			group_id = text[:len(text)-1]
+		}
 
-        snykTool.WriteConf(token, group_id)
+		snykTool.WriteConf(token, group_id)
+	case "token":
+		fmt.Println(snykTool.GetToken())
 
-    case "list-users":
-        result, err := snykTool.ListUsers(os.Args[2])
-        if err != nil {
-            log.Fatal(err)
-        }
-        snykTool.FormatUser(result)
-        
-    case "list-group-users":
-        result, err := snykTool.GetGroupMembers()
-        if err != nil {
-            log.Fatal(err)
-        }
-        snykTool.FormatUser(result)
-  
-    case "list-orgs":
-	    result, err := snykTool.GetOrgs()
-	    if err != nil {
-	        log.Fatal(err)
-        }
-        snykTool.FormatOrg(result)
+	case "list-users":
+		result, err := snykTool.ListUsers(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		snykTool.FormatUser(result)
+
+	case "list-group-users":
+		result, err := snykTool.GetGroupMembers()
+		if err != nil {
+			log.Fatal(err)
+		}
+		snykTool.FormatUser(result)
+
+	case "compare-users":
+		r1, err := snykTool.ListUsers(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		s1 := snykTool.GetOrgName(os.Args[2])
+		r2, err := snykTool.ListUsers(os.Args[3])
+		if err != nil {
+			log.Fatal(err)
+		}
+		s2 := snykTool.GetOrgName(os.Args[3])
+		snykTool.FormatUsers2Cols(r1, r2, s1, s2)
+	case "copy-users":
+		snykTool.CopyUsers(os.Args[2], os.Args[3])
+		fmt.Println("OK")
+
+	case "list-orgs":
+		result, err := snykTool.GetOrgs()
+		if err != nil {
+			log.Fatal(err)
+		}
+		snykTool.FormatOrg(result)
 
 	case "search-org":
 		result, err := snykTool.SearchOrgs(flag.Arg(1))
 		if err != nil {
-            log.Fatal(err)
-        }
-        snykTool.FormatOrg(result)
+			log.Fatal(err)
+		}
+		snykTool.FormatOrg(result)
 
 	case "create-org":
-        result, err := snykTool.CreateOrg(flag.Arg(1))
-        if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Printf("%s\n", result.Id)
+		result, err := snykTool.CreateOrg(flag.Arg(1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", result.Id)
 
 	case "list-projects":
-        result, err := snykTool.GetProjects(flag.Arg(1))
-        if err != nil {
-            log.Fatal(err)
-        }
-        snykTool.FormatProjects(result)
-        
-    case "search-projects":
-        result, err := snykTool.SearchProjects(flag.Arg(1), flag.Arg(2))
-        if err != nil {
-            log.Fatal(err)
-        }
-        snykTool.FormatProjects(result)
+		result, err := snykTool.GetProjects(flag.Arg(1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		snykTool.FormatProjects(result)
 
-    case "list-project-ignores":
-        res := snykTool.GetProjectIgnores(flag.Arg(1), flag.Arg(2))
-        snykTool.FormatProjectIgnore(res)
-       
-    case "list-org-ignores":
-        result, err := snykTool.GetProjects(flag.Arg(1))
-        if err != nil {
-            log.Fatal(err)
-        }
-        for _, prj := range result.Projects {
-            res := snykTool.GetProjectIgnores(flag.Arg(1), prj.Id)
-            if !snykTool.IsQuiet() {
-                fmt.Printf("====> %s\n", prj.Name)
-            }
-            snykTool.FormatProjectIgnore(res)
-        }
+	case "search-projects":
+		result, err := snykTool.SearchProjects(flag.Arg(1), flag.Arg(2))
+		if err != nil {
+			log.Fatal(err)
+		}
+		snykTool.FormatProjects(result)
 
-    case "list-group-ignores":
-        // doing sequential because of the rate limiting on snyk api
-        result, err := snykTool.GetOrgs()
-	    if err != nil {
-	        log.Fatal(err)
-        }
-        for _, org := range result.Orgs {
-            if ! snykTool.IsQuiet() {
-                fmt.Printf("<<< %s >>>\n", org.Name)
-            }
-            result_prj, err := snykTool.GetProjects(org.Id)
-            if err != nil {
-                log.Fatal(err)
-            }
-            for _, prj := range result_prj.Projects {
-                result_ignores := snykTool.GetProjectIgnores(org.Id, prj.Id)
-                if !snykTool.IsQuiet() {
-                    fmt.Printf("====> %s\n", prj.Name)
-                }
-                snykTool.FormatProjectIgnore(result_ignores)
-            }
-            // sleep for rate limit
-            time.Sleep(1 * time.Second)
-        }
-        
-    case "list-project-issues":
-        result, err := snykTool.GetProjectIssues(flag.Arg(1), flag.Arg(2))
-        if err != nil {
-            log.Fatal(err)
-        }
-        for _, issue := range(result.Issues) {
-            if *quietFlag {
-                fmt.Printf("%s\n", issue.Id)
-            } else {
-                fmt.Printf("%s\t%s\t%s\n", issue.Id, issue.PkgName, issue.IssueData.Severity)
-            }
-        }
-    case "compare-project-issues":
-        result1, err := snykTool.GetProjectIssues(flag.Arg(1), flag.Arg(2))
-        if err != nil {
-            log.Fatal(err)
-        }
-        result2, err := snykTool.GetProjectIssues(flag.Arg(3), flag.Arg(4))
-        if err != nil {
-            log.Fatal(err)
-        }
+	case "list-project-ignores":
+		res := snykTool.GetProjectIgnores(flag.Arg(1), flag.Arg(2))
+		snykTool.FormatProjectIgnore(res)
 
-        var res1, res2 []string
-        for _, issue := range(result1.Issues) {
-            res1 = append(res1, issue.Id)
-        }
-        sort.Sort(sort.Reverse(sort.StringSlice(res1)))
-        for _, issue := range(result2.Issues) {
-            res2 = append(res2, issue.Id)
-        }
-        sort.Sort(sort.Reverse(sort.StringSlice(res2)))
+	case "list-org-ignores":
+		result, err := snykTool.GetProjects(flag.Arg(1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, prj := range result.Projects {
+			res := snykTool.GetProjectIgnores(flag.Arg(1), prj.Id)
+			if !snykTool.IsQuiet() {
+				fmt.Printf("====> %s\n", prj.Name)
+			}
+			snykTool.FormatProjectIgnore(res)
+		}
 
-        s3 := merge(res1, res2)
-        for i := 0; i < len(s3); i++ {
-            if contains(res1, s3[i]) && contains(res2, s3[i]) {
-                fmt.Printf("%s\t\t\t\t%s\n", s3[i], s3[i])
-            } else if contains(res1, s3[i]) {
-                fmt.Printf("%s\t\t\t\t\t------MISSING------\n", s3[i])
-            } else if contains(res2, s3[i]) {
-                fmt.Printf("------MISSING------\t\t\t\t\t%s\n", s3[i])
-            } else {
-                fmt.Printf("===== ERROR ====\n")
-            }
-        }
+	case "list-group-ignores":
+		// doing sequential because of the rate limiting on snyk api
+		result, err := snykTool.GetOrgs()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, org := range result.Orgs {
+			if !snykTool.IsQuiet() {
+				fmt.Printf("<<< %s >>>\n", org.Name)
+			}
+			result_prj, err := snykTool.GetProjects(org.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, prj := range result_prj.Projects {
+				result_ignores := snykTool.GetProjectIgnores(org.Id, prj.Id)
+				if !snykTool.IsQuiet() {
+					fmt.Printf("====> %s\n", prj.Name)
+				}
+				snykTool.FormatProjectIgnore(result_ignores)
+			}
+			// sleep for rate limit
+			time.Sleep(1 * time.Second)
+		}
 
-    case "report-org-issues":
-        // get all prjs for the  org
-        // for each prj get all the issues
-        // count sevs
-        result, err := snykTool.GetProjects(flag.Arg(1))
-        if err != nil {
-            log.Fatal(err)
-        }
-        var h int
-        var m int
-        var l int
-        var prjs int
-        for _, project := range result.Projects {
-            prjs += 1
-            res, err := snykTool.GetProjectIssues(flag.Arg(1), project.Id)
-            if err != nil {
-                log.Fatal(err)
-            }
-            for _, issue := range(res.Issues) {
-                if "high" == issue.IssueData.Severity {
-                    h += 1
-                } else if "medium" == issue.IssueData.Severity {
-                    m += 1
-                } else {
-                    l += 1
-                }
-            }
-        }
-        fmt.Println("P:", prjs)
-        fmt.Println("H: ", h)
-        fmt.Println("M: ", m)
-        fmt.Println("L: ", l)
+	case "list-project-issues":
+		result, err := snykTool.GetProjectIssues(flag.Arg(1), flag.Arg(2))
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, issue := range result.Issues {
+			if *quietFlag {
+				fmt.Printf("%s\n", issue.Id)
+			} else {
+				fmt.Printf("%s\t%s\t%s\n", issue.Id, issue.PkgName, issue.IssueData.Severity)
+			}
+		}
+	case "compare-project-issues":
+		result1, err := snykTool.GetProjectIssues(flag.Arg(1), flag.Arg(2))
+		if err != nil {
+			log.Fatal(err)
+		}
+		result2, err := snykTool.GetProjectIssues(flag.Arg(3), flag.Arg(4))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var res1, res2 []string
+		for _, issue := range result1.Issues {
+			res1 = append(res1, issue.Id)
+		}
+		sort.Sort(sort.Reverse(sort.StringSlice(res1)))
+		for _, issue := range result2.Issues {
+			res2 = append(res2, issue.Id)
+		}
+		sort.Sort(sort.Reverse(sort.StringSlice(res2)))
+
+		s3 := merge(res1, res2)
+		for i := 0; i < len(s3); i++ {
+			if contains(res1, s3[i]) && contains(res2, s3[i]) {
+				fmt.Printf("%s\t\t\t\t%s\n", s3[i], s3[i])
+			} else if contains(res1, s3[i]) {
+				fmt.Printf("%s\t\t\t\t\t------MISSING------\n", s3[i])
+			} else if contains(res2, s3[i]) {
+				fmt.Printf("------MISSING------\t\t\t\t\t%s\n", s3[i])
+			} else {
+				fmt.Printf("===== ERROR ====\n")
+			}
+		}
+
+	case "report-org-issues":
+		// get all prjs for the  org
+		// for each prj get all the issues
+		// count sevs
+		result, err := snykTool.GetProjects(flag.Arg(1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		var h int
+		var m int
+		var l int
+		var prjs int
+		for _, project := range result.Projects {
+			prjs += 1
+			res, err := snykTool.GetProjectIssues(flag.Arg(1), project.Id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, issue := range res.Issues {
+				if "high" == issue.IssueData.Severity {
+					h += 1
+				} else if "medium" == issue.IssueData.Severity {
+					m += 1
+				} else {
+					l += 1
+				}
+			}
+		}
+		fmt.Println("P:", prjs)
+		fmt.Println("H: ", h)
+		fmt.Println("M: ", m)
+		fmt.Println("L: ", l)
 
 	}
 }
 
-
 func contains(s []string, x string) bool {
-    for _, v := range(s) {
-        if v == x {
-            return true
-        }
-    }
-    return false
+	for _, v := range s {
+		if v == x {
+			return true
+		}
+	}
+	return false
 
 }
 
-func merge(s1 []string, s2[]string) []string {
-    var s3 []string
-    s3 = s1
-    for i:= 0; i < len(s2); i++ {
-        if !contains(s1, s2[i]) {
-            s3 = append(s3, s2[i])
-        }
-    }
-    sort.Sort(sort.Reverse(sort.StringSlice(s3)))
-    return s3
-    
+func merge(s1 []string, s2 []string) []string {
+	var s3 []string
+	s3 = s1
+	for i := 0; i < len(s2); i++ {
+		if !contains(s1, s2[i]) {
+			s3 = append(s3, s2[i])
+		}
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(s3)))
+	return s3
+
 }
